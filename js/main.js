dbb3ca583268136ccc13395b586a537c6d7a81c9
Аладьин Антон <aladinanton@Air-Aladin.IGD_Rostelecom>
2024-03-10 14:12:15 +0700

    Create project

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$("#hover").hover(
+  function () {
+
+    $("#none").toggleClass("d-none")
+
+  }
+);
+
+$("#hover1").hover(
+  function () {
+
+    $("#none1").toggleClass("d-none")
+
+  }
+);
+
+$("#hover2").hover(
+  function () {
+
+    $("#none2").toggleClass("d-none")
+
+  }
+);
+
